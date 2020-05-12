0f67a3b4b8066d00ed90d27fb98a3a9acf27bedc
jp <jpjp8414@gmail.com>
2020-05-12 12:11:44 +0900

    Feat: User Data Mocking (Product Flavor)

---
+++ app/src/api/java/com.jp.boilerplate.data.datasource.remote/UserRemoteDataSource.kt
@@ -0,0 +1,29 @@
+package com.jp.boilerplate.data.datasource.remote
+
+import com.jp.boilerplate.data.datasource.UserDataSource
+import com.jp.boilerplate.data.datasource.api.UserAPI
+import com.jp.boilerplate.data.entity.User
+import com.jp.boilerplate.data.entity.Users
+import com.jp.boilerplate.util.NetworkUtil
+import io.reactivex.Completable
+import io.reactivex.Flowable
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+class UserRemoteDataSource : UserDataSource {
+
+    override fun getUsers(): Flowable<Users> {
+        return NetworkUtil.getAPI(UserAPI::class.java).getUser()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    override fun isCached(): Single<Boolean> {
+        throw UnsupportedOperationException()
+    }
+
+    override fun save(it: Users): Completable {
+        throw UnsupportedOperationException()
+    }
+}
--- app/src/mock/java/com.jp.boilerplate.data.datasource.remote/UserRemoteDataSource.kt
+++ app/src/mock/java/com.jp.boilerplate.data.datasource.remote/UserRemoteDataSource.kt
@@ -1,9 +1,8 @@
 package com.jp.boilerplate.data.datasource.remote
 
 import com.jp.boilerplate.data.datasource.UserDataSource
-import com.jp.boilerplate.data.datasource.api.UserAPI
 import com.jp.boilerplate.data.entity.User
-import com.jp.boilerplate.util.NetworkUtil
+import com.jp.boilerplate.data.entity.Users
 import io.reactivex.Completable
 import io.reactivex.Flowable
 import io.reactivex.Single
@@ -12,17 +11,21 @@ import io.reactivex.schedulers.Schedulers
 
 class UserRemoteDataSource : UserDataSource {
 
-    override fun getUser(): Flowable<User> {
-        return NetworkUtil.getAPI(UserAPI::class.java).getUser()
+    override fun getUsers(): Flowable<Users> {
+        return Flowable.defer { Flowable.just(mockingUsers()) }
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    private fun mockingUsers(): Users {
+        return mutableListOf(User("jp", 30), User("jh", 20), User("ar", 10))
     }
 
     override fun isCached(): Single<Boolean> {
         throw UnsupportedOperationException()
     }
 
-    override fun save(it: User): Completable {
+    override fun save(it: Users): Completable {
         throw UnsupportedOperationException()
     }
 }
